9b2537be78fb5ccb8ef97f8b775f62f1687bff63
Mostafatahadev1 <mostafatahadev1@gmail.com>
2026-03-03 04:25:36 +0200

    Part 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 1
+            #region A
+            // a =>  Composition
+            // Reason: Department cannot exist independently without the University.
+            // If the University is closed, all Departments are destroyed.
+            #endregion
+
+            #region B
+            // b =>  Association
+            // Reason: Driver uses a Car but does not own it.
+            // There is no whole-part relationship between them.
+            #endregion
+
+            #region C
+            // c => Inheritance
+            // Reason: A Dog is an Animal, which represents an IS-A relationship.
+            #endregion
+
+            #region D
+            // d =>  Aggregation
+            // Reason: Players can exist independently of the Team.
+            // If the Team is deleted, Players still exist.
+            #endregion
+
+            #region e
+            // e =>  Dependency
+            // Reason: Logger is used only as a method parameter temporarily
+            // and is not stored as a field. This represents the weakest relationship.
+            #endregion
+
+            #endregion
         }
     }
 }
