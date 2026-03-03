90ae17c2ee2e3d652137260096bf361830d406e9
Mostafatahadev1 <mostafatahadev1@gmail.com>
2026-03-03 04:18:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_session_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
